56e1e40ffe871df488428fdcef8c17193cc60135
calvinyjl <calvinlu.luc@gmail.com>
2025-02-28 12:55:31 +0000

    changed same line

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World2222222");
         System.out.println("djsalhdasl");
         System.out.println("dsajkhdasjakdjahdjwka");
         System.out.println("please");
